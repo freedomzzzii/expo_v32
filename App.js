7253590945bd558b757bf7c54afe6b7321832b46
freedomzzzii <theeraporn.tia@gmail.com>
2019-03-13 17:37:08 +0700

    [update] add provider redux on app

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
+import { Provider } from 'react-redux';
 
-import { AppNavigator } from './navigation';
+import AppNavigator from './navigation/AppNavigator';
+
+import configureStore from './configureStore';
+const store = configureStore();
 
 export default class App extends React.Component {
   state = {
@@ -18,14 +22,16 @@ export default class App extends React.Component {
           onFinish={this._handleFinishLoading}
         />
       );
-    } else {
-      return (
+    }
+
+    return (
+      <Provider store={store}>
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <AppNavigator />
         </View>
-      );
-    }
+      </Provider>
+    );
   }
 
   _loadResourcesAsync = async () => {
@@ -35,20 +41,13 @@ export default class App extends React.Component {
         require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
-        // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ]);
   };
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
+  _handleLoadingError = error => console.warn(error);
 
   _handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true });
