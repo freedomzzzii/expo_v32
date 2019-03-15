d8658fd1047878133c711f4827c015488e5faf99
freedomzzzii <theeraporn.tia@gmail.com>
2019-03-15 17:22:36 +0700

    [create] file lang on home screen

---
+++ screens/HomeScreen/LangHomeScreen.js
@@ -0,0 +1,12 @@
+const lang = {
+  en: {
+    foo: 'Foo',
+    bar: 'Bar {{someValue}}',
+  },
+  fr: {
+    foo: 'como telle fous',
+    bar: 'chatouiller {{someValue}}',
+  },
+};
+
+export default lang;
