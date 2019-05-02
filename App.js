21a8a2bfb7426df7369ac8d180803a5b709c300a
freedomzzzii <theeraporn.tia@gmail.com>
2019-05-02 10:50:07 +0700

    [update] style app and fix warning

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import { Provider } from 'react-redux';
+
+import styles from './StyleApp';
 
 import AppNavigator from './navigation/AppNavigator';
 
@@ -9,9 +11,29 @@ import configureStore from './configureStore';
 const store = configureStore();
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadingComplete: false,
+    };
+  }
+
+  _loadResourcesAsync = async () => (
+    Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        ...Icon.Ionicons.font,
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ])
+  );
+
+  _handleLoadingError = error => console.warn(error);
+
+  _handleFinishLoading = () => this.setState({ isLoadingComplete: true });
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -33,30 +55,4 @@ export default class App extends React.Component {
       </Provider>
     );
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        ...Icon.Ionicons.font,
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => console.warn(error);
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
