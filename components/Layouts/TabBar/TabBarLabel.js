6745e422449c8ff614ac7ebc27b9a23e9cde93b1
freedomzzzii <theeraporn.tia@gmail.com>
2019-05-07 15:04:59 +0700

    [create] component tab bar label

---
+++ components/Layouts/TabBar/TabBarLabel.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './StyleTabBar';
+
+import { getItem } from '../../../helpers';
+import { Colors } from '../../../config';
+
+export default class TabBarLabel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: null,
+    };
+  }
+
+  async UNSAFE_componentWillMount() {
+    const language = await getItem('language');
+    this.setState({ language });
+  }
+
+  han
+
+  render() {
+    const { language } = this.state;
+    const { content, index, color } = this.props;
+    
+    if (!language || !content || !index) {
+      return null;
+    }
+    return(
+      <Text style={{ ...styles.label, color }}>{content[language][index]}</Text>
+    );
+  }
+}
+
+TabBarLabel.propTypes = {
+  content: PropTypes.shape({
+    th: PropTypes.shape({}).isRequired,
+    en: PropTypes.shape({}).isRequired,
+  }).isRequired,
+  index: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
--- components/index.js
+++ components/index.js
@@ -1,13 +1,18 @@
-import { MonoText } from './Style/StyledText';
-
-import TabBarIcon from './TabBarIcon/TabBarIcon';
-
+// layout
 import Loading from './Layouts/Loading/Loading';
+
+import TabBarIcon from './Layouts/TabBar/TabBarIcon';
+
+import TabBarLabel from './Layouts/TabBar/TabBarLabel';
+
+// component
+import { MonoText } from './Style/StyledText';
 
 export {
   // layout
   Loading,
+  TabBarIcon,
+  TabBarLabel,
   // component
   MonoText,
-  TabBarIcon,
 };
