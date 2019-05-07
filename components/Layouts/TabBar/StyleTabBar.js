9d516db26367f124cf9f75c2fb522f20d79a049e
freedomzzzii <theeraporn.tia@gmail.com>
2019-05-07 15:04:48 +0700

    [create] component tab bar icon

---
+++ components/Layouts/TabBar/StyleTabBar.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  label: {
+    fontSize: 9,
+    fontFamily: 'prompt',
+    marginTop: 5,
+    textAlign: 'center',
+  },
+  icon: {
+    marginBottom: -3,
+  },
+});
---
+++ components/Layouts/TabBar/TabBarIcon.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './StyleTabBar';
+
+import { Colors } from '../../../config';
+import { Icon } from '../../../helpers';
+
+export default class TabBarIcon extends Component {
+  render() {
+    const { focused, name } = this.props;
+
+    return (
+      <Icon
+        name={name}
+        size={26}
+        style={styles.icon}
+        color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      />
+    );
+  }
+}
+
+TabBarIcon.propTypes = {
+  name: PropTypes.string,
+  focused: PropTypes.bool,
+  source: PropTypes.any,
+  style: PropTypes.shape({}),
+  img: PropTypes.bool,
+};
